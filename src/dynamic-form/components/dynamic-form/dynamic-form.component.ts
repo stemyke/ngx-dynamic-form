1044d4e9a3b09979adef186be843757f6eec12ae
stemyke <h064179@stud.u-szeged.hu>
2018-03-20 23:04:02 +0100

    Base structure

---
+++ src/dynamic-form/components/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: "dynamic-form",
+    templateUrl: "./dynamic-form.component.html"
+})
+export class DynamicFormComponent {
+
+}
---
+++ src/dynamic-form/dynamic-form.module.ts
@@ -0,0 +1,54 @@
+import {ModuleWithProviders, NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {DynamicFormComponent} from "./components";
+
+// --- Components ---
+export const components = [
+    DynamicFormComponent
+];
+
+// --- Directives ---
+export const directives = [
+];
+
+// --- Pipes ---
+export const pipes = [
+];
+
+
+
+@NgModule({
+    declarations: [
+        ...components,
+        ...directives,
+        ...pipes
+    ],
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    exports: [
+        ...components,
+        ...directives,
+        ...pipes
+    ],
+    providers: [
+
+    ]
+})
+export class DynamicFormModule {
+    static forChild(): ModuleWithProviders {
+        return {
+            ngModule: DynamicFormModule,
+            providers: pipes
+        };
+    }
+
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: DynamicFormModule,
+            providers: []
+        }
+    }
+}
