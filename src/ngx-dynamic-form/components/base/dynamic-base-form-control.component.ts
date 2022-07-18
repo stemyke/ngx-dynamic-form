b169c469c67daa13142b111b89d42720c2fb5e1d
stemy <balazs.stemler@metrix.co.hu>
2022-07-18 15:35:57 +0200

    onValueChanged callback in FormControlComponent.
    v13.1.15

--- src/ngx-dynamic-form/components/base/dynamic-base-form-control.component.ts
+++ src/ngx-dynamic-form/components/base/dynamic-base-form-control.component.ts
@@ -1,5 +1,15 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, Output} from "@angular/core";
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    EventEmitter,
+    Input,
+    OnDestroy,
+    Output
+} from "@angular/core";
 import {FormGroup} from "@angular/forms";
+import {debounceTime, Subscription} from "rxjs";
 import {
     DynamicFormControlComponent,
     DynamicFormControlLayout,
@@ -14,7 +24,7 @@ import {
     template: "",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DynamicBaseFormControlComponent<T extends DynamicFormControlModel> extends DynamicFormControlComponent {
+export class DynamicBaseFormControlComponent<T extends DynamicFormControlModel> extends DynamicFormControlComponent implements AfterViewInit, OnDestroy {
 
     @Input() formLayout: DynamicFormLayout;
     @Input() group: FormGroup;
@@ -25,10 +35,27 @@ export class DynamicBaseFormControlComponent<T extends DynamicFormControlModel> 
     @Output() change: EventEmitter<any>;
     @Output() focus: EventEmitter<any>;
 
+    protected subscription: Subscription;
+
     constructor(readonly cdr: ChangeDetectorRef, layoutService: DynamicFormLayoutService, validationService: DynamicFormValidationService) {
         super(layoutService, validationService);
         this.blur = new EventEmitter<any>();
         this.change = new EventEmitter<any>();
         this.focus = new EventEmitter<any>();
+    }
+
+    ngAfterViewInit(): void {
+        this.subscription = this.control.valueChanges.pipe(debounceTime(500)).subscribe(value => {
+            this.onValueChanged(value);
+        });
+    }
+
+    ngOnDestroy(): void {
+        if (!this.subscription) return;
+        this.subscription.unsubscribe();
+    }
+
+    protected onValueChanged(value: any): void {
+
     }
 }
