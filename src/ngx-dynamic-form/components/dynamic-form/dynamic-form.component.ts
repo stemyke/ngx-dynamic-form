35556c1a1d5d2aba6032e64ba028c6d42c4f2f93
stemy <stemler.balazs.akos@gmail.com>
2025-06-12 18:37:01 +0200

    Run change detection on form fields, when language is changed.
    v19.2.10

--- src/ngx-dynamic-form/components/dynamic-form/dynamic-form.component.ts
+++ src/ngx-dynamic-form/components/dynamic-form/dynamic-form.component.ts
@@ -2,17 +2,19 @@ import {
     ChangeDetectionStrategy,
     Component,
     computed,
+    effect,
     inject,
     Injector,
     input,
     output,
-    resource,
     ViewEncapsulation
 } from "@angular/core";
-import {rxResource} from "@angular/core/rxjs-interop";
+import {rxResource, toSignal} from "@angular/core/rxjs-interop";
 import {FormGroup} from "@angular/forms";
 import {Subject} from "rxjs";
 import {FormlyFormOptions} from "@ngx-formly/core";
+import {EventsService, LANGUAGE_SERVICE} from "@stemy/ngx-utils";
+
 import {FormFieldChangeEvent, FormFieldConfig, IDynamicForm} from "../../common-types";
 import {DynamicFormBuilderService} from "../../services/dynamic-form-builder.service";
 
@@ -27,6 +29,10 @@ export class DynamicFormComponent implements IDynamicForm {
 
     protected readonly builder = inject(DynamicFormBuilderService);
 
+    protected readonly events = inject(EventsService);
+
+    protected readonly languages = inject(LANGUAGE_SERVICE);
+
     readonly labelPrefix = input<string>("label");
 
     readonly data = input<any>({});
@@ -35,20 +41,15 @@ export class DynamicFormComponent implements IDynamicForm {
 
     readonly fieldChanges = new Subject<FormFieldChangeEvent>();
 
-    protected readonly config$ = resource({
-        request: () => ({
-            fields: this.fields(),
-            labelPrefix: this.labelPrefix()
-        }),
-        loader: async p => {
-            const {fields, labelPrefix} = p.request;
-            return fields || this.builder.resolveFormFields(this.data()?.constructor, null, {
-                labelPrefix
-            });
-        }
+    protected readonly language = toSignal(this.events.languageChanged, {
+        initialValue: this.languages.currentLanguage
     });
 
-    readonly config = computed(() => this.config$.value());
+    readonly config = computed(() => {
+        return this.fields() || this.builder.resolveFormFields(this.data()?.constructor, null, {
+            labelPrefix: this.labelPrefix()
+        });
+    });
 
     readonly group = computed(() => {
         this.config();
@@ -57,7 +58,8 @@ export class DynamicFormComponent implements IDynamicForm {
 
     protected readonly status$ = rxResource({
         request: () => this.group(),
-        loader: p => p.request.statusChanges
+        loader: p => p.request.statusChanges,
+        defaultValue: "PENDING"
     });
 
     readonly status = computed(() => this.status$.value());
@@ -70,6 +72,16 @@ export class DynamicFormComponent implements IDynamicForm {
             injector: inject(Injector)
         }
     };
+
+    constructor() {
+        effect(() => {
+            this.language();
+            this.config().forEach(field => {
+                if (!field.options) return;
+                this.options.detectChanges(field);
+            });
+        });
+    }
 
     submit() {
         // TODO: Templ disable submit
