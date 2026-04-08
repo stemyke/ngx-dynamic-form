b902f812decdfac324fa0bc4fda333579f4aebee
stemy <balazs.stemler@metrix.co.hu>
2026-04-08 11:41:31 +0200

    Use Angulars inject method in ModelUtils because ngx-utils now supports runInInjectionContext.
    v19.9.1

--- src/ngx-dynamic-form/models/utils.ts
+++ src/ngx-dynamic-form/models/utils.ts
@@ -1,9 +1,10 @@
-import {FactoryDependencies, API_SERVICE, IApiService, ILanguageService, LANGUAGE_SERVICE} from "@stemy/ngx-utils";
+import {inject} from "@angular/core";
+import {LANGUAGE_SERVICE} from "@stemy/ngx-utils";
 import {FormSelectOptionsFactory} from "../common-types";
 
 export class ModelUtils {
-    @FactoryDependencies(LANGUAGE_SERVICE)
-    static getLanguages(language: ILanguageService): FormSelectOptionsFactory {
+    static getLanguages(): FormSelectOptionsFactory {
+        const language = inject(LANGUAGE_SERVICE)
         return async () => {
             return language.languages.map(id => {
                 return {id, label: id};
